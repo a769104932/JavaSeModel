fe80a918ced570dfb4d154a2c51bf3a4c0ad2d3e
lug <a769104932@qq.com>
2021-01-27 09:37:24 +0800

    源文件

---
+++ src/pers/xfz/zpy/HelloWorld.java
@@ -0,0 +1,13 @@
+package pers.xfz.zpy;
+
+/**
+ * 2 * @Author: 卢广
+ * 3 * @Date: 2021/1/27 9:27
+ * 4
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello pain");
+
+    }
+}
