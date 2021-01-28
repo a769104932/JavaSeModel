4b07428b741d977c8505870c9b6ca5b2b0a9758a
lug <a769104932@qq.com>
2021-01-28 18:10:35 +0800

    浮点类型注意事项

--- src/pers/xfz/zpy/FloatDemo.java
+++ src/pers/xfz/zpy/FloatDemo.java
@@ -6,5 +6,27 @@ package pers.xfz.zpy;
  * 4
  */
 public class FloatDemo {
-
+    public static void main(String[] arr){
+        // 浮点数存在精度丢失.要注意
+        double x = 1.0 / 10;
+        double y = 1 / 10;
+        System.out.println(x);
+        System.out.println(y);
+        // 如果浮点数参与运算会自动提升为浮点数
+        int n = 5;
+        double d = 1.2 + 24.0 / n;
+        System.out.println(d);
+        // 某种情况也不会出现自动提升例如两个整数进行运算
+        int a = 5;
+        double f = 1.5 + 24 / a;
+        System.out.println(f);
+        // 小数转为整数需要注意会丢舍弃小数部分
+        double doubleDate = 23.9;
+        int intDate = (int)doubleDate;
+        System.out.println(intDate);
+        // 小数通过四舍五入转为整数只需要再加上0.5强转即可
+        double cc = 32.5;
+        intDate = (int)(cc+0.5);
+        System.out.println(intDate);
+    }
 }
